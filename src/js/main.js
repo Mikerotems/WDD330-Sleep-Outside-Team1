1a3bc265ee08697611dd4f3e600cbd25123d00d4
mike_rotems <mymaster17@gmail.com>
2026-09-21 15:53:36 +0100

    week2-v2

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-
 const dataSource = new ProductData("tents");
 
 const element = document.querySelector(".product-list");
 
 const productlist = new ProductList("tents", dataSource, element);
 
-productlist.init(); 
+productlist.init();
