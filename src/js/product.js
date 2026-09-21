8d04e0b23b2acb3a5c8e4f3456887c9de875c3aa
mike_rotems <mymaster17@gmail.com>
2026-09-21 14:53:20 +0100

    team2

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,13 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   const cartItems = getLocalStorage("so-cart") || [];
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,3 +21,8 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+function getParam(params){
+  const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const product = urlParams.get('product');
+}
