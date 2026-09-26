ccd0223618f2ddcc53ca5254820b446afd4168d6
mike_rotems <mymaster17@gmail.com>
2026-09-26 08:25:36 +0100

    build

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,5 +3,8 @@ import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
 
-const cart = new ShoppingCart("so-cart", document.querySelector(".product-list"));
+const cart = new ShoppingCart(
+  "so-cart",
+  document.querySelector(".product-list"),
+);
 cart.init();
